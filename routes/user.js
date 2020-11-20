2a65d46ae248b2e91e9092fb274d6b041ebb0bcc
git-k <kgunu93@gmail.com>
2020-11-20 23:42:44 +0900

    restapi 기본 구현

--- routes/user.js
+++ routes/user.js
@@ -7,87 +7,117 @@ const Op = models.Sequelize.Op;
 
 // 특정사용자로 정보조회 ex) 특정 사용자 상세 정보
 router.get("/:loginid", async (req, res, next) => {
-  const param = req.params;
+    const param = req.params;
 
-  try {
-    const user = await user_tb.findOne({ where: param });
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const user = await user_tb.findOne({ where: param });
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
 });
 
 // 사용자 정보를 조회할때
 router.get("/", async (req, res, next) => {
-  const search = req.body;
+    const search = req.body;
 
-  try {
-    const user = await user_tb.findAll({ where: search });
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const user = await user_tb.findAll({ where: search });
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
 });
 
 // 사용자 추가
 router.post("/", async (req, res, next) => {
-  const signUpUser = req.body;
-  const loginid = signUpUser.loginid;
-  const email = signUpUser.email;
-  const usernm = signUpUser.usernm;
-  const pwd = signUpUser.pwd;
+    const signUpUser = req.body;
+    const loginid = signUpUser.loginid;
+    const email = signUpUser.email;
+    const usernm = signUpUser.usernm;
+    const pwd = signUpUser.pwd;
 
-  try {
-    const user = await user_tb.findOne({
-      /*where: {
+    try {
+        const user = await user_tb.findOne({
+            /*where: {
         _id: {
           [Op.or]: [12, 13],
         },
       },*/
-      where: { [Op.or]: [{ loginid: loginid }, { email: email }] },
-      //where: { loginid: loginid },
-    });
-    throw Error("user exist");
-    if (user) {
-      throw Error("user exist");
+            where: { [Op.or]: [{ loginid: loginid }, { email: email }] },
+            //where: { loginid: loginid },
+        });
+
+        if (user) {
+            throw Error("user exist");
+        }
+
+        const success = await user_tb.create({
+            usernm: usernm,
+            loginid: loginid,
+            email: email,
+            pwd: pwd,
+        });
+
+        res.send(success);
+    } catch (err) {
+        next(err);
     }
-
-    const success = await user_tb.create({
-      usernm: usernm,
-      loginid: loginid,
-      email: email,
-      pwd: pwd,
-    });
-
-    res.send(success);
-  } catch (err) {
-    next(err);
-  }
 });
 
+// 전체 변경시 사용
 router.put("/", async (req, res, next) => {
-  /*await user_tb.create({
-    usernm: "관리자",
-    loginid: "admin",
-    email: "test@test",
-    pwd: "123",
-  });*/
-
-  const user = await user_tb.findAll({});
-  console.log(user);
-  res.json(user);
+    res.json({});
 });
 
+// 일부 내역 수정시 사용
 router.patch("/", async (req, res, next) => {
-  const user = await user_tb.findAll({});
-  console.log(user);
-  res.json(user);
+    const updateUser = req.body.user;
+    const usernm = updateUser.usernm;
+    const loginid = updateUser.loginid;
+    const email = updateUser.email;
+
+    try {
+        const user = await user_tb.findOne({
+            where: { [Op.or]: [{ loginid: loginid }, { email: email }] },
+        });
+
+        if (!user) {
+            throw Error("user no exist");
+        }
+
+        const success = await user_tb.update(
+            { usernm: usernm },
+            { where: { _id: user._id } }
+        );
+
+        res.json(success);
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.delete("/", async (req, res, next) => {
-  const user = await user_tb.findAll({});
-  console.log(user);
-  res.json(user);
+    const deleteUser = req.body;
+    const usernm = deleteUser.usernm;
+    const loginid = deleteUser.loginid;
+    const email = deleteUser.email;
+
+    try {
+        const user = await user_tb.findOne({
+            where: { [Op.or]: [{ loginid: loginid }, { email: email }] },
+        });
+
+        if (!user) {
+            throw Error("user no exist");
+        }
+
+        const success = await user_tb.destroy({ where: { _id: user._id } });
+
+        res.json(success);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
